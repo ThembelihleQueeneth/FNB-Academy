a6c06f70c05488ad46d1e199104ef4624640e259
ThembelihleQueeneth <malukathembelihle95@gmail.com>
2026-07-19 10:22:35 +0200

    feat: add digital ticket counter application

---
+++ concert_ticket_booker.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+artist = input("Enter the artist's name: ")
+
+print("\n==============================")
+print(f"Hey {name}! Your tickets to see {artist} have been booked successfully!l")
